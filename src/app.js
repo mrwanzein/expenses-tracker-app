2856b0f7574af2c6fa4f0d4000f43b526a89c950
Mrwan Zein <mrwanzein@outlook.com>
2018-09-16 13:14:56 -0400

    Removed dummy data

--- src/app.js
+++ src/app.js
@@ -3,20 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './redux/store/configureStore';
-import {addExpenses} from './redux/actions/expenses';
-import getVisibleExpenses from './redux/selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
 const store = configureStore();
-
-store.dispatch(addExpenses({ description: 'Water Bill', amount: 25000 }));
-store.dispatch(addExpenses({ description: 'Gas Bill', amount: 16000 }));
-store.dispatch(addExpenses({ description: 'Rent', amount: 109500 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log((visibleExpenses));
 
 const jsx = (
     <Provider store={store}>
