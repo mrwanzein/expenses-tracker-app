278ddd4ad3697d321bd909968061b4ba17ccb714
Mrwan Zein <mrwanzein@outlook.com>
2018-09-17 12:17:16 -0400

    Setup webpack production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,56 @@
 // entry -> ouput
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/, //files that end with .js
-            loader: 'babel-loader',
-            exclude: /node_modules/
+
+module.exports = (env) => {
+const isProductoin = env === 'production';
+const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return  {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
         },
-        {
-            test: /\.s?css/, // set the rule, import in app.js, serve
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+        module: {
+            rules: [{
+                test: /\.js$/, //files that end with .js
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css/, // set the rule, import in app.js, serve
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProductoin ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    }; 
 };
+
 
 // Loaders : for example, using babel through webpack
 
